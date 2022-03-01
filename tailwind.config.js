9631239f6c66a926c45f1ca27291c8d9b0c03c09
Vadim Savin <savinvadim1312@gmail.com>
2022-03-01 20:53:44 +0000

    don't prune tailwind from content folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   purge: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
+    './content/**/components/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
