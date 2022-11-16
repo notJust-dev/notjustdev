1b93bce94f24eeb79625ee0ce799b495437f4380
Vadim Savin <savinvadim1312@gmail.com>
2022-11-16 09:43:03 +0000

    tacking

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,12 +5,38 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          {/* Can we already delete it? */}
           <meta
             name="facebook-domain-verification"
             content="ybnih1fjzxz7h3ib5g8deqvb49hz79"
           />
         </Head>
         <body>
+          {/* Yandex.Metrika counter */}
+
+          <noscript>
+            <div>
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                src="https://mc.yandex.ru/watch/91208801"
+                style="position:absolute; left:-9999px;"
+                alt=""
+              />
+            </div>
+          </noscript>
+          {/* /Yandex.Metrika counter */}
+
+          {/* <!-- Google Tag Manager (noscript) --> */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-58HCJGW"
+              height="0"
+              width="0"
+              style="display:none;visibility:hidden"
+            ></iframe>
+          </noscript>
+          {/* <!-- End Google Tag Manager (noscript) --> */}
+
           <Main />
           <NextScript />
         </body>
