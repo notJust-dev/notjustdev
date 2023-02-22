8baa87ff6076f459efba753125fa77223bb208ca
Vadim Savin <savinvadim1312@gmail.com>
2023-02-22 14:19:04 +0000

    fixed links

--- src/pages/links/index.tsx
+++ src/pages/links/index.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import MaxWidthWrapper from '../../components/MaxWidthWrapper';
 import logo from '../../../public/images/logo/white.png';
 import { mainLinks, socialLinks, CustomLinkData } from '../../data/links';
+import pattern from '../../../public/images/brand_elements/Pattern_13_white.png';
 
 const CustomLink = ({ text, Icon, href }: CustomLinkData) => (
   <Link
@@ -14,35 +15,47 @@ const CustomLink = ({ text, Icon, href }: CustomLinkData) => (
       <Icon className="text-2xl text-custom-blue-500" />
     </div>
 
-    {text}
+    <h3>
+      <span className="text-gray-400">notJust </span>
+      {text}
+    </h3>
   </Link>
 );
 
 const FormSuccess = () => (
-  <MaxWidthWrapper
-    maxWidth={400}
-    className="h-screen flex flex-col justify-center"
-  >
-    <section className="bg-custom-blue-500 p-5 w-full flex flex-col ">
-      <Image
-        src={logo}
-        alt="notJust Development"
-        width={150}
-        className="self-center my-5"
-      />
-      {mainLinks.map((link) => (
-        <CustomLink key={link.text} {...link} />
-      ))}
-    </section>
+  <>
+    <Image
+      src={pattern}
+      alt=""
+      width={100}
+      height={200}
+      className="absolute bottom-0 right-0"
+    />
+    <MaxWidthWrapper
+      maxWidth={400}
+      className="h-screen flex flex-col justify-center"
+    >
+      <section className="bg-custom-blue-500 p-5 w-full flex flex-col ">
+        <Image
+          src={logo}
+          alt="notJust Development"
+          width={150}
+          className="self-center my-5"
+        />
+        {mainLinks.map((link) => (
+          <CustomLink key={link.text} {...link} />
+        ))}
+      </section>
 
-    <section className="mt-2 self-center flex flex-row">
-      {socialLinks.map((link) => (
-        <Link key={link.name} href={link.href} className="m-2">
-          <link.Icon className="text-2xl text-gray-200 hover:text-secondary" />
-        </Link>
-      ))}
-    </section>
-  </MaxWidthWrapper>
+      <section className="mt-2 self-center flex flex-row">
+        {socialLinks.map((link) => (
+          <Link key={link.name} href={link.href} className="m-2">
+            <link.Icon className="text-2xl text-gray-200 hover:text-secondary" />
+          </Link>
+        ))}
+      </section>
+    </MaxWidthWrapper>
+  </>
 );
 
 export default FormSuccess;
