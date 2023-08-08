3bc171e25b841a96136f07bf7720e3359de5a8cd
Vadim Savin <savinvadim1312@gmail.com>
2023-08-08 12:21:51 +0100

    reduce the size of MDXImages

--- src/components/MDXImage.tsx
+++ src/components/MDXImage.tsx
@@ -25,8 +25,8 @@ const MDXImage = ({ src, alt, children, width, height }: Props) => (
 MDXImage.defaultProps = {
   src: '',
   alt: '',
-  width: 1920,
-  height: 1080,
+  width: 1280,
+  height: 720,
 } as Partial<Props>;
 
 export default MDXImage;
