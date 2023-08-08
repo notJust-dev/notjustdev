7910a54fd89255b8a5c1e75f2c9f10094f30bc72
Vadim Savin <savinvadim1312@gmail.com>
2023-08-08 12:36:24 +0100

    add sizes of MDXImages

--- src/components/MDXImage.tsx
+++ src/components/MDXImage.tsx
@@ -9,18 +9,21 @@ interface Props {
   height?: number | string;
 }
 
-const MDXImage = ({ src, alt, children, width, height }: Props) => (
-  <span className="relative block my-7 text-center">
-    <Image
-      src={src as string}
-      alt={alt as string}
-      width={Number(width)}
-      height={Number(height)}
-      unoptimized={src?.endsWith('.gif')}
-    />
-    {children}
-  </span>
-);
+const MDXImage = ({ src, alt, children, width, height }: Props) => {
+  return (
+    <span className="relative block my-7 text-center">
+      <Image
+        src={src as string}
+        alt={alt as string}
+        width={Number(width)}
+        height={Number(height)}
+        unoptimized={src?.endsWith('.gif')}
+        sizes="(max-width: 1100px) 100vw, 1100px" // TODO: it doesn't account for 2 columns layout of content + table of contents
+      />
+      {children}
+    </span>
+  );
+};
 
 MDXImage.defaultProps = {
   src: '',
