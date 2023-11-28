4f68648045343a6c0e9b22d8016ac47f6cd7a799
Vadim Savin <savinvadim1312@gmail.com>
2023-11-28 15:09:51 +0000

    remove BF deal

--- src/components/Announcement/Announcement.tsx
+++ src/components/Announcement/Announcement.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link';
-import MaxWidthWrapper from '../MaxWidthWrapper';
+// import Link from 'next/link';
+// import MaxWidthWrapper from '../MaxWidthWrapper';
 // import { useCallback, useEffect, useState } from 'react';
 
 const Announcement = () => {
@@ -26,34 +26,34 @@ const Announcement = () => {
   // // let height = 300 ->  300 - scrollY -> 100
   // let height = Math.max(300 - scrollY, 100);
 
-  // return null;
+  return null;
 
-  return (
-    <div
-      className="justify-center p-3 py-7 sticky top-0 z-50"
-      style={{ backgroundColor: '#F3EAC0' }}
-    >
-      <MaxWidthWrapper>
-        <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between">
-          <div>
-            <div
-              className={`text-custom-blue-500 transition-all text-lg lg:text-2xl`}
-            >
-              <b>🚨 Black Friday Deal: 50% off </b> on our Academy courses 🎓
-            </div>
-          </div>
+  // return (
+  //   <div
+  //     className="justify-center p-3 py-7 sticky top-0 z-50"
+  //     style={{ backgroundColor: '#F3EAC0' }}
+  //   >
+  //     <MaxWidthWrapper>
+  //       <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between">
+  //         <div>
+  //           <div
+  //             className={`text-custom-blue-500 transition-all text-lg lg:text-2xl`}
+  //           >
+  //             <b>🚨 Black Friday Deal: 50% off </b> on our Academy courses 🎓
+  //           </div>
+  //         </div>
 
-          <div className="mt-5 sm:mt-0">
-            <Link href="https://academy.notjust.dev/">
-              <span className="shadow-md rounded-md font-bold text-lg lg:text-xl py-2 lg:py-4 px-5 lg:px-10 bg-custom-blue-500 text-primary ">
-                View Offer
-              </span>
-            </Link>
-          </div>
-        </div>
-      </MaxWidthWrapper>
-    </div>
-  );
+  //         <div className="mt-5 sm:mt-0">
+  //           <Link href="https://academy.notjust.dev/">
+  //             <span className="shadow-md rounded-md font-bold text-lg lg:text-xl py-2 lg:py-4 px-5 lg:px-10 bg-custom-blue-500 text-primary ">
+  //               View Offer
+  //             </span>
+  //           </Link>
+  //         </div>
+  //       </div>
+  //     </MaxWidthWrapper>
+  //   </div>
+  // );
 };
 
 export default Announcement;
