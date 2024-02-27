a69430bcf61954b0fe600825ebf3e8f3df5b0104
Vadim Savin <savinvadim1312@gmail.com>
2024-02-27 12:43:38 +0000

    remove annoucnement

--- src/components/Announcement/Announcement.tsx
+++ src/components/Announcement/Announcement.tsx
@@ -2,8 +2,8 @@
 // import MaxWidthWrapper from '../MaxWidthWrapper';
 // import { useCallback, useEffect, useState } from 'react';
 
-import Link from 'next/link';
-import MaxWidthWrapper from '../MaxWidthWrapper';
+// import Link from 'next/link';
+// import MaxWidthWrapper from '../MaxWidthWrapper';
 
 const Announcement = () => {
   // const [minimized, setMinimized] = useState(false);
@@ -29,29 +29,56 @@ const Announcement = () => {
   // // let height = 300 ->  300 - scrollY -> 100
   // let height = Math.max(300 - scrollY, 100);
 
-  return (
-    <div className="justify-center p-3 py-7 sticky top-0 z-50 bg-gradient-to-br from-indigo-950 to-purple-800">
-      <MaxWidthWrapper>
-        <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between">
-          <div>
-            <div className={`text-gray-100 transition-all text-md lg:text-lg`}>
-              We are looking for the next projects for{' '}
-              <b>notJust.Incubator</b>
-              🚀
-            </div>
-          </div>
+  // return (
+  //   <div className="justify-center p-3 py-7 sticky top-0 z-50 bg-gradient-to-br from-indigo-950 to-purple-800">
+  //     <MaxWidthWrapper>
+  //       <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between">
+  //         <div>
+  //           <div className={`text-gray-100 transition-all text-md lg:text-lg`}>
+  //             We are looking for the next projects for{' '}
+  //             <b>notJust.Incubator</b>
+  //             🚀
+  //           </div>
+  //         </div>
 
-          <div className="mt-5 sm:mt-0">
-            <Link href="/incubator">
-              <span className="shadow-md rounded-md font-bold text-md lg:text-lg py-2 lg:py-3 px-4 lg:px-7 text-custom-blue-500 bg-primary ">
-                Read more
-              </span>
-            </Link>
-          </div>
-        </div>
-      </MaxWidthWrapper>
-    </div>
-  );
+  //         <div className="mt-5 sm:mt-0">
+  //           <Link href="/incubator">
+  //             <span className="shadow-md rounded-md font-bold text-md lg:text-lg py-2 lg:py-3 px-4 lg:px-7 text-custom-blue-500 bg-primary ">
+  //               Read more
+  //             </span>
+  //           </Link>
+  //         </div>
+  //       </div>
+  //     </MaxWidthWrapper>
+  //   </div>
+  // );
+  return null;
 };
 
 export default Announcement;
+
+// Incubator announcement
+
+// return (
+//   <div className="justify-center p-3 py-7 sticky top-0 z-50 bg-gradient-to-br from-indigo-950 to-purple-800">
+//     <MaxWidthWrapper>
+//       <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between">
+//         <div>
+//           <div className={`text-gray-100 transition-all text-md lg:text-lg`}>
+//             We are looking for the next projects for{' '}
+//             <b>notJust.Incubator</b>
+//             🚀
+//           </div>
+//         </div>
+
+//         <div className="mt-5 sm:mt-0">
+//           <Link href="/incubator">
+//             <span className="shadow-md rounded-md font-bold text-md lg:text-lg py-2 lg:py-3 px-4 lg:px-7 text-custom-blue-500 bg-primary ">
+//               Read more
+//             </span>
+//           </Link>
+//         </div>
+//       </div>
+//     </MaxWidthWrapper>
+//   </div>
+// );
