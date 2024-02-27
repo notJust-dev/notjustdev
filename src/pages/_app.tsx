9129b9029023dc9d3228b3beef8e00ecffe5b853
Vadim Savin <savinvadim1312@gmail.com>
2024-02-27 18:43:36 +0000

    small fixes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,20 +5,6 @@ import '../styles/globals.css';
 import { GtmScript } from '../components/GTM';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
-// export function reportWebVitals({
-//   id,
-//   name,
-//   label,
-//   value,
-// }: NextWebVitalsMetric) {
-//   event(name, {
-//     category: label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
-//     value: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
-//     label: id, // id unique to current page load
-//     nonInteraction: true, // avoids affecting bounce rate.
-//   });
-// }
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
