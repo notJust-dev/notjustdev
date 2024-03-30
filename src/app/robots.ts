72e051f9a4c2c98acc5745f101aa1d27275f9709
Vadim Savin <savinvadim1312@gmail.com>
2024-03-30 12:03:37 +0000

    added robots

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    // sitemap: 'https://acme.com/sitemap.xml',
+  };
+}
