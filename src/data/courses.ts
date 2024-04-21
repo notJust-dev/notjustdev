4589c00cf36ded0017e517a7be29548fa2e9a50a
Vadim Savin <savinvadim1312@gmail.com>
2024-04-21 12:46:30 +0100

    add seoTitle for course

--- src/data/courses.ts
+++ src/data/courses.ts
@@ -4,6 +4,7 @@ const courses: PostMeta[] = [
     slug: 'the-full-stack-mobile-developer',
     updatedOn: '2023-02-01T20:11:06.794Z',
     title: 'The Full Stack Mobile Developer',
+    seoTitle: 'The Full Stack Mobile Developer Course | notJust Academy',
     image: '/images/thumbnails/courses/the-full-stack-mobile-developer.png',
     description:
       'Join the Full Stack Mobile Developer course and learn the full lifecycle of developing mobile applications. Master both frontend and backend skills by building complex real-world projects with React Native and AWS Amplify',
