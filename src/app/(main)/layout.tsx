b5e0dc6ff7e38c93e1ac44fa25b3be85d3cc23d2
Vadim Savin <savinvadim1312@gmail.com>
2025-04-16 20:49:47 +0200

    header background blur image

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,10 +1,21 @@
 import ServerLayout from '@/components/ServerLayout';
 import '@/styles/globals.css';
+import Image from 'next/image';
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <ServerLayout>
-      <section>{children}</section>
-    </ServerLayout>
+    <>
+      <Image
+        src="/images/brand_elements/union.svg"
+        alt="brand elements"
+        width={731}
+        height={850}
+        className="absolute top-0 bottom-0 left-auto right-0 blur-[300px]"
+      />
+
+      <ServerLayout>
+        <section>{children}</section>
+      </ServerLayout>
+    </>
   );
 }
