b5c0d763d30a15271417b270e3374dd392cf173b
Vadim Savin <savinvadim1312@gmail.com>
2025-04-18 14:08:09 +0200

    moved to app folder: email preferences updated

---
+++ src/app/(main)/email-preferences-updated/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+import MaxWidthWrapper from '@/components/MaxWidthWrapper';
+
+const FormSuccess = () => (
+  <MaxWidthWrapper>
+    <section className="p-5 self-stretch flex flex-col items-center min-h-[80vh] justify-center">
+      <Image src="/images/checkmark.png" width={96} height={96} alt="" />
+      <h1>Email preferences updated!</h1>
+      <p className="my-1">We appreciate your digital personal space 🙌</p>
+    </section>
+  </MaxWidthWrapper>
+);
+
+export default FormSuccess;
