1c284b2434355c421bc47688b5537ea849fb8787
Vadim Savin <savinvadim1312@gmail.com>
2025-04-18 16:38:18 +0200

    removed pages folder

--- src/components/shared/YoutubeVideo/YoutubeVideo.tsx
+++ src/components/shared/YoutubeVideo/YoutubeVideo.tsx
@@ -1,14 +1,9 @@
-// import LiteYouTubeEmbed from 'react-lite-youtube-embed';
-
 interface Props {
   id: string;
   title: string;
 }
 
-function YoutubeVideo({ id, title }: Props) {
-  // return (
-  //   <LiteYouTubeEmbed id={id} title={title} adNetwork={true} cookie={true} />
-  // );
+export default function YoutubeVideo({ id, title }: Props) {
   return (
     <iframe
       src={`https://www.youtube.com/embed/${id}`}
@@ -20,5 +15,3 @@ function YoutubeVideo({ id, title }: Props) {
     />
   );
 }
-
-export default YoutubeVideo;
