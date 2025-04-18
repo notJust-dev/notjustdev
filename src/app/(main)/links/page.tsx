e507824c42ceb927db5e73849c6484548ad54bfa
Vadim Savin <savinvadim1312@gmail.com>
2025-04-18 15:53:10 +0200

    moved to app folder: links

--- src/app/(main)/links/page.tsx
+++ src/app/(main)/links/page.tsx
@@ -1,11 +1,11 @@
+'use client';
 import Image from 'next/image';
 import Link from 'next/link';
 import React, { useState } from 'react';
-import MaxWidthWrapper from '../../components/MaxWidthWrapper';
-import logo from '../../../public/images/logo/white.png';
-import { mainLinks, socialLinks, CustomLinkData } from '../../data/links';
-import pattern from '../../../public/images/brand_elements/Pattern_13_white.png';
-import NewsletterPopup from '../../components/NewsletterForm/NewsletterModal';
+import MaxWidthWrapper from '@/components/MaxWidthWrapper';
+import logo from '../../../../public/images/logo/white.png';
+import { mainLinks, socialLinks, CustomLinkData } from '@/data/links';
+import NewsletterPopup from '@/components/NewsletterForm/NewsletterModal';
 import { BsNewspaper } from 'react-icons/bs';
 
 const CustomLink = ({
@@ -16,17 +16,17 @@ const CustomLink = ({
 }: CustomLinkData & { onClick?: () => void }) => (
   <Link
     href={href}
-    className="border-2 hover:text-secondary border-secondary  m-2 h-14  flex flex-row items-center"
+    className="border hover:text-primary border-primary  m-2 h-14  flex flex-row items-center"
     onClick={onClick}
   >
-    <div className="bg-secondary self-stretch flex items-center justify-center w-12 mr-3">
-      <Icon className="text-2xl text-custom-blue-500" />
+    <div className="bg-primary self-stretch flex items-center justify-center w-12 mr-3">
+      <Icon className="text-2xl text-black" />
     </div>
 
-    <h3>
-      <span className="text-gray-400">notJust </span>
+    <span className="text-xl font-medium font-space-grotesk">
+      <span className="text-white-200 font-normal">notJust </span>
       {text}
-    </h3>
+    </span>
   </Link>
 );
 
@@ -35,18 +35,11 @@ const LinksPage = () => {
 
   return (
     <>
-      <Image
-        src={pattern}
-        alt=""
-        width={100}
-        height={200}
-        className="absolute bottom-0 right-0"
-      />
       <MaxWidthWrapper
-        maxWidth={400}
+        maxWidth={600}
         className="h-screen flex flex-col justify-center"
       >
-        <section className="bg-custom-blue-500 p-5 w-full flex flex-col ">
+        <section className="border border-white-100/25 ro p-5 px-20 w-full flex flex-col ">
           <Image
             src={logo}
             alt="notJust Development"
@@ -69,7 +62,7 @@ const LinksPage = () => {
         <section className="mt-2 self-center flex flex-row">
           {socialLinks.map((link) => (
             <Link key={link.name} href={link.href} className="m-2">
-              <link.Icon className="text-2xl text-gray-200 hover:text-secondary" />
+              <link.Icon className="text-2xl text-gray-200 hover:text-primary" />
             </Link>
           ))}
         </section>
